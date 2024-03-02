6c56562ecf8bb01b63d7afc1603503fd8325a210
chpsmstr <prakashankkit@gmail.com>
2024-03-01 22:39:20 -0800

    Removed HTML style tags and replaced with equivalent CSS.

--- eduquest/app/Register/page.tsx
+++ eduquest/app/Register/page.tsx
@@ -9,20 +9,27 @@ export default function Register() {
         alt="Eduquest logo"
         className="h-100 w-100 xs:h-80 xs:w-80 lg:h-100 lg:w-100"
       ></img>
-      <div className="text-center text-3xl lg:text-3xl lg:mx-4 sm:mx-2">Are you a?</div>
-      <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-2 sm:grid-cols-2 mt-6">
-        <Link href="/TeacherRegistration">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2" style={{backgroundColor: "red", padding: "12px"}}>Teacher</h1>
-        </Link>
-        <Link href="/StudentRegistration">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2" style={{backgroundColor: "blue", padding: "12px"}}>Student</h1>
-        </Link>
-        
+      <div className="text-center text-3xl lg:text-3xl lg:mx-4 sm:mx-2">
+        Are you a
       </div>
-      
-      <Link href="/Login">
-          <h1 className="text-center text-2xl lg:text-2xl lg:mx-4 sm:mx-2 mt-6">Already have an account</h1>
+      <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-2 sm:grid-cols-2 mt-6">
+        <Link href="/Register/TeacherRegistration">
+          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 bg-red-600 p-4 rounded-md ">
+            Teacher
+          </h1>
         </Link>
+        <Link href="/Register/StudentRegistration">
+          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 p-4 bg-indigo-800 rounded-md ">
+            Student
+          </h1>
+        </Link>
+      </div>
+
+      <Link href="/Login">
+        <h1 className="text-center text-2xl lg:text-2xl lg:mx-4 sm:mx-2 mt-6">
+          Already have an account?
+        </h1>
+      </Link>
     </main>
   );
 }
