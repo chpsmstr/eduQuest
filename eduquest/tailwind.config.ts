1097132cec5a9c9a11d2b76b25297a93b6540889
Mark <mark.lovesey03@gmail.com>
2024-03-04 23:15:18 -0800

    Added EduQuest favicon & implemented new designs for light and dark mode homepage background.

--- eduquest/tailwind.config.ts
+++ eduquest/tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        landing: "url('../public/bgpxArt.png')",
+        landing: "url('../public/bglight.webp')",
       },
     },
   },
