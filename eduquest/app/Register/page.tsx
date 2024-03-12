ee5d1e6166090bb44203fbf0fe88c0e00d4cf52b
Acr0batOG <grhys2001@gmail.com>
2024-03-12 14:03:30 -0700

    Updated the login and registration pages based on TA's comments

--- eduquest/app/Register/page.tsx
+++ eduquest/app/Register/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Register() {
   return (
-    <main className="px-9 bg-gradient-to-b from-amber-100 to-amber-500 h-screen flex items-center flex-col">
+    <main className="px-4 bg-gradient-to-b from-amber-100 to-amber-500 min-h-screen flex items-center flex-col">
       <img
         src="/eduQuestpxArtLogo.png"
         alt="Eduquest logo"
@@ -14,19 +14,19 @@ export default function Register() {
       </div>
       <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-2 sm:grid-cols-2 mt-6">
         <Link href="/Register/TeacherRegistration">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 bg-red-600 p-4 rounded-md ">
+          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 p-4 hover:border-red-600 border-red-700 hover:bg-red-500  bg-red-600 rounded-md" style={{borderWidth: "4px"}}>
             Teacher
           </h1>
         </Link>
         <Link href="/Register/StudentRegistration">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 p-4 bg-indigo-800 rounded-md ">
+          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2 p-4 hover:border-indigo-600 border-indigo-700 hover:bg-indigo-500  bg-indigo-600 rounded-md" style={{borderWidth: "4px"}}>
             Student
           </h1>
         </Link>
       </div>
 
       <Link href="/Login">
-        <h1 className="text-center text-2xl lg:text-2xl lg:mx-4 sm:mx-2 mt-6">
+        <h1 className="text-center text-2xl lg:text-2xl lg:mx-4 hover:text-blue-600 sm:mx-2 mt-6">
           Already have an account?
         </h1>
       </Link>
