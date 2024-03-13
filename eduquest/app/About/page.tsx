f5e52ee968721f1446306e36adf174b697aca0f4
Mark <mark.lovesey03@gmail.com>
2024-03-13 03:03:42 -0700

    updated homepage & worked on dynamic route dashboard

---
+++ eduquest/app/About/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    (
+        <main>
+            
+        </main>
+    )
+}
--- eduquest/app/page.tsx
+++ eduquest/app/page.tsx
@@ -5,21 +5,32 @@ export default function Home() {
   return (
     <main className="px-9 bg-cover  bg-center bg-landing h-screen flex items-center flex-col">
       <img
-        src="/eduQuestpxArtLogo.png"
+        src="/eduQuestlogoresized2.webp"
         alt="Eduquest logo"
-        className="h-100 w-100 xs:h-80 xs:w-80 lg:h-100 lg:w-100"
+        className="h-100 w-100 xs:h-80 xs:w-80 lg:h-100 lg:w-100 "
       ></img>
-      <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-3 sm:grid-cols-3">
-        <Link href="">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2">Educator</h1>
-        </Link>
-        <Link href="/Register">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2">Register</h1>
-        </Link>
-        <Link href="/Login">
-          <h1 className="text-3xl lg:text-4xl lg:mx-4 sm:mx-2">Start</h1>
+      <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-2 sm:grid-cols-2 gap-5"> 
+      <Link href="/Dashboard">
+          <h1 className="text-4xl lg:text-4xl lg:mx-4 sm:mx-2 transition duration-500 hover:scale-125 rounded">Enter</h1>
+        </Link> 
+        
+        <Link href="/About">
+          <h1 className="text-4xl lg:text-4xl lg:mx-4 sm:mx-2 transition duration-500 hover:scale-125">About</h1>
         </Link>
       </div>
+      <br></br><br></br>
+      <div className="grid text-center space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-2 lg:grid-cols-2 gap-6"> 
+      <Link href="/Register">
+      <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-6 rounded">
+          Sign Up
+          </button>
+        </Link>
+        <Link href="/Login">
+        <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-7 rounded">
+          Log In
+          </button>
+        </Link>
+        </div>
     </main>
   );
 }
