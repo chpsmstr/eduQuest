a5395a91cbfa58d8411165df87b20a4869e4ef6c
Ankkit <prakashankkit@gmail.com>
2024-03-15 15:23:46 -0700

    Added Subpage authorization

--- eduquest/app/UserPages/Teacher/Subteacher/page.tsx
+++ eduquest/app/UserPages/Teacher/Subteacher/page.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default async function page() {
 
-  return <div>TEACHER PAGE PLACEHOLDER</div>;
+  return <div>SUB TEACHER PAGE PLACEHOLDER</div>;
 }
--- eduquest/app/page.tsx
+++ eduquest/app/page.tsx
@@ -1,22 +1,18 @@
 import Image from "next/image";
 import MenuButton from "./Components/MenuButton";
 import Logo from "./Components/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="px-9 bg-center bg-cover bg-landing h-screen flex items-center flex-col">
       <div className="grid text-right space-y-4 sm:space-y-0 lg:space-y-0  lg:grid-cols-1 lg:grid-cols-1 gap-3 justify-items-end">
         <br></br>
-        <Link href="/Register">
-          <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-6 rounded">
-            Sign Up
-          </button>
-        </Link>
-        <Link href="/Login">
-          <button className="bg-gray-500 hover:bg-gray-700 text-white py-2 px-7 rounded">
-            Log In
-          </button>
-        </Link>
+      
+
+
+        <MenuButton params={{ text: "Register", href: "/Register" }} />
+        <MenuButton params={{ text: "Login", href: "/Login" }} />
       </div>
       <div className="grid text-right space-y-4 sm:space-y-0 lg:space-y-0 lg:grid-cols-1 lg:grid-cols-1 gap-3 justify-items-center">
       <img
