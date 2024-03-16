a8d95853c11fb7838119eb1a82435f9e0f4fe053
chpsmstr <prakashankkit@gmail.com>
2024-03-15 18:49:43 -0700

    Converted Classbox into a component

---
+++ eduquest/app/Components/ClassBox.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+type Params = {
+  params: {
+    color: string;
+    link: string;
+    label: string;
+  };
+};
+
+export default function ClassBox({ params }: Params) {
+  return (
+    <div className="relative mb-4 w-56 sm:w-56 rounded">
+      <Link href={`${params?.link}`}>
+        <div className="block h-56 cursor-pointer transition duration-300 ease-in-out transform hover:brightness-90">
+          {/* Colored half at the top */}
+          <div
+            className={`absolute left-0 top-0 w-full h-1/2 ${params?.color} transition duration-300 ease-in-out transform hover:brightness-110`}
+          ></div>
+          {/* White lower half */}
+          <div className="absolute left-0 bottom-0 w-full h-1/2 bg-white"></div>
+        </div>
+      </Link>
+      {/* Text position within class*/}
+      <div
+        className="absolute top-0 left-0 w-full p-2 text-black text-2xl font-bold pointer-events-none"
+        style={{ marginTop: "calc(1/6 * 6rem)" }}
+      >
+        {params?.label}
+      </div>
+    </div>
+  );
+}
