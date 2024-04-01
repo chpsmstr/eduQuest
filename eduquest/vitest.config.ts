a575991ebd3010af12c9268f32fac801f9795d18
Daniel Penner <danielwpenner@shaw.ca>
2024-03-31 19:27:18 -0700

    Fixed database testing
    just comment out the include in vitest.config when testing db functionality (recomment when committing)

--- eduquest/vitest.config.ts
+++ eduquest/vitest.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./__tests__/setup.ts"]
+    setupFiles: ["./__tests__/backend/setup.ts"],
+    //Delete the "//" below when testing database
+    include: ["./__tests__/frontend/*"],
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./") }],
