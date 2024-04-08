32423da21e42bf461bd6f08cbf1ee3d782f8920e
chpsmstr <prakashankkit@gmail.com>
2024-04-08 15:19:58 -0700

    Sessionals set up

--- eduquest/app/UserPages/Student/page.tsx
+++ eduquest/app/UserPages/Student/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import { options } from "@/app/api/auth/[...nextauth]/options";
+import { cookies } from 'next/headers';
 
 export default async function page() {
   const session = await getServerSession(options);
 
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/server");
-  }
+  // if (!session) {
+  //   redirect("/api/auth/signin?callbackUrl=/server");
+  // }
+  const cookieStore =  cookies();
+  const role = await cookieStore.get('role')?.value;
+  console.log(role);
   return <div>STUDENT PAGE PLACEHOLDER</div>;
 }
--- eduquest/app/UserPages/Teacher/layout.tsx
+++ eduquest/app/UserPages/Teacher/layout.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
-import { options } from "@/app/api/auth/[...nextauth]/options";
+// import { getServerSession } from "next-auth/next";
+// import { redirect } from "next/navigation";
+// import { options } from "@/app/api/auth/[...nextauth]/options";
+import { cookies } from 'next/headers';
 
 export default async function DashboardLayout({
     children, // will be a page or nested layout
   }: {
     children: React.ReactNode
   }) {
-    const session = await getServerSession(options);
-
-  if (session?.user.role !== "teacher") {
+    // const session = await getServerSession(options);
+    const cookieStore = cookies()
+    const role = cookieStore.get('role')?.value;
+    const id = cookieStore.get('id')?.value;
+  if (role !== "teacher") {
     return <h1 className="text-5xl">Access Denied</h1>;
   }
 
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/UserPages/Teacher/");
+  if (!id) {
+    // redirect("/api/auth/signin?callbackUrl=/UserPages/Teacher/");
+    <div>
+    Not logged in
+      </div>
   }
   return <div>{children}</div>;
     
