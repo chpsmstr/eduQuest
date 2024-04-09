5abd10f8ba93ea1eb7d950b2e22ec46d047d04fe
Mark <mark.lovesey03@gmail.com>
2024-04-08 21:49:23 -0700

    made background of about page elements darker

--- eduquest/app/About/page.tsx
+++ eduquest/app/About/page.tsx
@@ -15,11 +15,11 @@ export default function About() {
       <div className="mt-8 text-center">
         <h1 className="text-5xl mb-8">Team Members</h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 text-white">
-          <div className="text-3xl rounded-lg bg-blue-300 px-6 py-3">Ankkit Prakash</div>
-          <div className="text-3xl rounded-lg bg-blue-300 px-6 py-3">Daniel Penner</div>
-          <div className="text-3xl rounded-lg bg-blue-300 px-6 py-3">Mark Lovesey</div>
-          <div className="text-3xl rounded-lg bg-blue-300 px-6 py-3">Nolan Nishikawa</div>
-          <div className="text-3xl rounded-lg bg-blue-300 px-6 py-3">Rhys Smith</div>
+          <div className="text-3xl rounded-lg bg-blue-500 px-6 py-3">Ankkit Prakash</div>
+          <div className="text-3xl rounded-lg bg-blue-500 px-6 py-3">Daniel Penner</div>
+          <div className="text-3xl rounded-lg bg-blue-500 px-6 py-3">Mark Lovesey</div>
+          <div className="text-3xl rounded-lg bg-blue-500 px-6 py-3">Nolan Nishikawa</div>
+          <div className="text-3xl rounded-lg bg-blue-500 px-6 py-3">Rhys Smith</div>
         </div>
       </div>
       {/* Team Pixel Art */}
@@ -27,10 +27,10 @@ export default function About() {
         <img src="/pixelteam.webp" alt="Picture of a Controller" className="object-scale-down h-48 w-96" />
       </div>
       {/* Mission Statement */}
-      <div className="mt-12 text-center">
+      <div className="mt-8 text-center">
         <h1 className="text-5xl mb-4">Mission Statement</h1>
         <div className="mx-auto max-w-2xl text-white">
-          <p className="text-3xl rounded-lg bg-blue-300 px-6 py-3">
+          <p className="text-3xl rounded-lg bg-blue-500 px-6 py-3">
             EduQuest is an innovative e-learning platform created by team members for our Software Engineering Course. We hope that this platform can benefit both teachers and students alike in creating a fun learning environment.
           </p>
         </div>
